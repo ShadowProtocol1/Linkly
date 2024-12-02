d1774069f97d4a0c934ba2978bac14f87b16a386
ShadowProtocol1 <uietcseyayaativerma2125@kuk.ac.in>
2024-12-02 09:57:08 +0000

    changes

--- components/Navbar.js
+++ components/Navbar.js
@@ -6,7 +6,7 @@ const Navbar = () => {
   return (
     <nav className='h-16 bg-purple-700 flex justify-between px-3 items-center text-white '>
       <div className="logo font-bold text-2xl"> 
-        <Link href="/">BitLinks</Link>
+        <Link href="/">Linkly</Link>
       </div>
       <ul className='flex justify-center gap-4 items-center'>
         <Link href="/"><li>Home</li></Link>
